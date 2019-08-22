bf98857653bf1eb185eeb246f5fd469c81dee5a9
3llomi <allomi.raj@gmail.com>
2019-08-22 18:52:27 +0200

    deleted unnecessary files

--- Example/build/Pods.build/Debug-iphonesimulator/iRecordView.build/DerivedSources/iRecordView_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char iRecordViewVersionString[];
- extern const double iRecordViewVersionNumber;
-
- const unsigned char iRecordViewVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:iRecordView  PROJECT:Pods-1" "\n";
- const double iRecordViewVersionNumber __attribute__ ((used)) = (double)1.;
